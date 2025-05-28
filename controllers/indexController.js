e7d38e9ac772ce490dcf07771fd58c66fe8a5e49
amaagomez <agomezdisanto@udesa.edu.ar>
2025-05-28 16:00:04 -0300

    ondex controller

---
+++ controllers/indexController.js
@@ -0,0 +1,32 @@
+let db = require("../database/models");
+let op = db.Sequelize.Op;
+
+let indexController = {
+  home: function (req, res) {
+    db.Producto.findAll({
+      include: [{ association: "usuario" }, { association: "comentarios" }],
+    }).then(function (data) {
+      // res.send (data)
+      res.render("index", { productos: data });
+    });
+  },
+
+  search: function (req, res) {
+    db.Producto.findAll({
+      where: {
+        nombre: { [op.like]: req.query.search },
+      },
+
+      include: [{ association: "usuario" }, { association: "comentarios" }],
+    }).then(function (data) {
+      // res.send (data)
+
+      res.render("search-results", {
+        productos: data,
+        busqueda: req.query.search,
+      });
+    });
+  },
+};
+
+module.exports = indexController;
