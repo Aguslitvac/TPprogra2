9d6cb3ad1732ab20e986fbf619a0c272786fc37f
amaagomez <agomezdisanto@udesa.edu.ar>
2025-06-02 15:32:20 -0300

    datbase

--- database/config/config.js
+++ database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
-    "database": "TPProg2",
+    "database": "tpprog2",
     "host": "127.0.0.1",
     "dialect": "mysql", 
     "port": 3306
